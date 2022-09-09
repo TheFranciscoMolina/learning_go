d15c7a95eea60be5b060c3ceb24c02f43c2029e1
Francisco Molina <fdm.molina@gmail.com>
2022-09-08 21:36:57 -0300

    Update mapas.go

--- 01 - Estructura de Datos/Mapas/mapas.go
+++ 01 - Estructura de Datos/Mapas/mapas.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+
+	//Son similares a los objetos literales de Javascript. Tienen clave y valor
+	//Declaro la var, uso la fn make
+	//Creo un "mapa" y se asigna primero el tipo de valor que lleva la clave, y despues el tipo del valor
+	dias := make(map[int]string)
+
+	fmt.Println(dias)
+
+}
