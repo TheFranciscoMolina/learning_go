b95976881877d0cf7e897b5eea990aea1dc77d5e
Francisco Molina <fdm.molina@gmail.com>
2022-09-12 13:31:50 -0300

    Update operadoresenasignacion.go

--- 02 - Control de flujos/Operadores en asignacion/operadoresenasignacion.go
+++ 02 - Control de flujos/Operadores en asignacion/operadoresenasignacion.go
@@ -1,14 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	a := 2
 	//Suma en asignacion:
 	a += 2 /* (es como decir que a = a+2) */
+	fmt.Println(a)
 	//Resta
 	a -= 1
+	fmt.Println(a)
 	//Multiplicacion
 	a *= 4
+	fmt.Println(a)
 	//Dvision:
 	a /= 2
+	fmt.Println(a)
+	//Modulo
+	a %= 2
+	fmt.Println(a)
 }
