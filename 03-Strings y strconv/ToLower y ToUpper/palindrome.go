25475e20835dca67e4653da2142acc8e703f6573
Francisco Molina <fdm.molina@gmail.com>
2022-09-13 18:06:25 -0300

    Update palindrome.go

--- 03-Strings y strconv/ToLower y ToUpper/palindrome.go
+++ 03-Strings y strconv/ToLower y ToUpper/palindrome.go
@@ -5,12 +5,30 @@ import (
 	"strings"
 )
 
-func esPalindromo(palabra string) {
-	palabra = strings.ToLower(palabra)
+func rev(texto string) string {
+	arrTexto := strings.Split(texto, "")
+	arrRev := make([]string, 0)
 
+	for i := len(arrTexto) - 1; i >= 0; i-- {
+		arrRev = append(arrRev, arrTexto[i])
+	}
+
+	return strings.Join(arrRev, "")
+}
+
+func esPalindromo(palabra string) bool {
 	fmt.Println(palabra)
+	palabra = strings.ToLower(palabra)
+	palabra = strings.ReplaceAll(palabra, " ", "")
+	palabraRev := rev(palabra)
+	fmt.Println(palabraRev)
+	return palabra == palabraRev
 }
 
 func main() {
-	esPalindromo("ya sabia")
+	if esPalindromo("oso") {
+		fmt.Println("Es palindromo!")
+	} else {
+		fmt.Println("No es palindromo")
+	}
 }
