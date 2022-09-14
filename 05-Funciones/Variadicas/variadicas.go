05006b3efb7f1d0f759144759e70933f0ac5864e
Francisco Molina <fdm.molina@gmail.com>
2022-09-13 23:47:56 -0300

    Update variadicas.go

--- 05-Funciones/Variadicas/variadicas.go
+++ 05-Funciones/Variadicas/variadicas.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import "fmt"
+
+//Usando ... decimos que los argumentos son indefinidos
+func sumar(numeros ...int) int {
+	var total int
+
+	//Ignoro el indice con _,
+	//y uso el valor num
+	for _, num := range numeros {
+		total += num
+	}
+	return total
+}
+
+func main() {
+	result := sumar(5 + 5 + 5)
+	fmt.Println(result)
+}
