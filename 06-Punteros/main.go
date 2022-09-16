7e5303b0912177f6e53531be408821a421425668
Francisco Molina <fdm.molina@gmail.com>
2022-09-16 00:06:54 -0300

    Update main.go

--- 06-Punteros/main.go
+++ 06-Punteros/main.go
@@ -12,7 +12,7 @@ func main() {
 	palabra := "Desde main!"
 	fmt.Printf("%p\n", &palabra)
 	fmt.Println("Antes de la fn: ", palabra)
-	modificarValor(&palabra)
+	modificarValor(&palabra) //Se pasa la referencia a la variable
 	fmt.Println("Despues de la fn: ", palabra)
 
 }
