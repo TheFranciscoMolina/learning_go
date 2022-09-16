6c21aaeda55a63b2c1231ded4d2e337bf5119309
Francisco Molina <fdm.molina@gmail.com>
2022-09-16 19:21:24 -0300

    Update main.go

--- 05-Funciones/Manejo de Errores/main.go
+++ 05-Funciones/Manejo de Errores/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+)
+
+func dividir(dividendo, divisor int) (int, error) {
+	if divisor == 0 {
+		return 0, errors.New("No es posible dividir entre cero. Ingresa otro numero")
+	} else {
+		return dividendo / divisor, nil
+	}
+
+}
+
+func main() {
+	resultado, error := dividir(10, 2)
+
+	if error == nil {
+		fmt.Println("El resultado es:", resultado)
+	} else {
+		fmt.Println(error)
+	}
+}
