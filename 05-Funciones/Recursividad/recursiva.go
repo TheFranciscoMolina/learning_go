05d345c35424f3048a33f1b1498b6725a3ed01c9
Francisco Molina <fdm.molina@gmail.com>
2022-09-15 23:22:12 -0300

    Create recursiva.go

---
+++ 05-Funciones/Recursividad/recursiva.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	fac := n * factorial(n-1)
+	return fac
+}
+
+func main() {
+	fmt.Println(factorial(6))
+}
