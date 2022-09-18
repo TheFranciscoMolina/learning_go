1ce82e026b15b9049d0bdb9920e90a3f32fded28
Francisco Molina <fdm.molina@gmail.com>
2022-09-17 22:11:47 -0300

    Update main.go

--- 08-Modularizacion y Paquetes/main.go
+++ 08-Modularizacion y Paquetes/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "paquetes/saludar"
+import (
+	"fmt"
+	"paquetes/models"
+	// "paquetes/saludar"
+)
 
 func main() {
-	saludar.Hola()
+	// saludar.Hola()
+	persona1 := models.Persona{}
+	persona1.Constructor("Francisco", 30)
+	fmt.Println(persona1)
 
 }
